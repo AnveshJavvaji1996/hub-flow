94f5851f875d30b93dabd9fed4734c8f0c7d14d8
Anvesh Javvaji <anveshjavvaji6991@gmail.com>
2026-08-30 15:57:45 +0530

    modified app.js

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const chalk = require('chalk');
 console.log(chalk.green("Hello from my Node app running inside GitHub Actions!"));
 console.log(chalk.blue("Node version:"), process.version);
 console.log(chalk.yellow("Environment:"), process.env.APP_ENV);
-console.log(chalk.magenta("Secret value exists:"), process.env.MY_SECRET ? "Yes" : "No");
+console.log(chalk.magenta("Secret value exists:"), process.env.MY_SECRET);
